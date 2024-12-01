adb33ace2fd4b492bbe5cb5150c2672c6c7d020b
Bruno Santos <7022432+dunderbruno@users.noreply.github.com>
2024-12-01 15:22:19 -0300

    solution of day 1 part 1

---
+++ 01/solution.py
@@ -0,0 +1,16 @@
+# Part 1
+with open("input.txt", "r") as input_file:
+    numbers = input_file.readlines()
+
+first = [int(i.split()[0]) for i in numbers]
+second = [int(i.split()[1]) for i in numbers]
+
+first.sort()
+second.sort()
+
+distances = []
+for i in range(len(first)):
+    distances.append(abs(first[i]-second[i]))
+
+total_distance = sum(distances)
+print(f'Total distance: {total_distance}')
